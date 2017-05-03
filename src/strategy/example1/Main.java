c2d76a3e3fbc270f099bb984acd88402ded92b39
DongYuHui <dyh920827@gmail.com>
2017-05-03 16:31:12 +0800

    添加策略模式说明

---
+++ src/strategy/example1/Main.java
@@ -0,0 +1,15 @@
+package strategy.example1;
+
+/**
+ * 策略模式，示例 1
+ * Created on 2017/5/3 16:15
+ *
+ * @author DongYuHui at <a href="mailto:dyh920827@hotmail.com">dyh920827@hotmail.com</a>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Example1");
+    }
+
+}
---
+++ src/strategy/example2/Main.java
@@ -0,0 +1,15 @@
+package strategy.example2;
+
+/**
+ * 策略模式，示例 2
+ * Created on 2017/5/3 16:15
+ *
+ * @author DongYuHui at <a href="mailto:dyh920827@hotmail.com">dyh920827@hotmail.com</a>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("example2");
+    }
+
+}
