90dc9e0e4edc64ed5d1d6515d07f814296befa51
DongYuHui <dyh920827@gmail.com>
2017-05-16 21:33:15 +0800

    完善装饰者模式

---
+++ src/decorator/Main.java
@@ -0,0 +1,12 @@
+package decorator;
+
+/**
+ * FixMe
+ * ---
+ * All Rights Reserved by Author
+ * Created on 2017/05/16 21:28
+ *
+ * @author DongYuHui at <a href="dyh920827@gmail.com">dyh920827@gmail.com</a>
+ */
+public class Main {
+}
---
+++ src/decorator/Whip.java
@@ -0,0 +1,12 @@
+package decorator;
+
+/**
+ * FixMe
+ * ---
+ * All Rights Reserved by Author
+ * Created on 2017/05/16 21:27
+ *
+ * @author DongYuHui at <a href="dyh920827@gmail.com">dyh920827@gmail.com</a>
+ */
+public class Whip {
+}
